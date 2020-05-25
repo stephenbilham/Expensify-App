45d5c47f2723558a88c70164b265a318b1002251
stephen bilham <stephenbilham@yahoo.com>
2020-05-25 10:56:06 -0400

    add expense summary to dashboard

--- public/src/components/ExpenseListItem.js
+++ public/src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import numeral from "numeral";
+import moment from "moment";
 
 //Export a stateless functional component
 // discription, amount, createAt;
@@ -9,7 +11,8 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    {amount} - {createdAt}
+    {numeral(amount / 100).format("$0,0.00")} -{" "}
+    {moment(createdAt).format("MMMM Do, YYYY")}
   </div>
 );
 
